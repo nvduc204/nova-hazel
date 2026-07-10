ae24a4a5128e34a38cea9d1b6728de6eb54610b1
NVD576 <duw3210@gmail.com>
2026-07-10 10:37:20 +0700

    fix mine-type

--- main.py
+++ main.py
@@ -131,11 +131,14 @@ def upload_delta(articles_data, store_name):
             operation = client.file_search_stores.upload_to_file_search_store(
                 file=filepath,
                 file_search_store_name=store_name,
-                config={"display_name": title_slug}
+                config={"display_name": title_slug,
+                "mime_type":"text/markdown"},
             )
             while not operation.done:
                 time.sleep(2)
                 operation = client.operations.get(operation)
+                print("loading...")
+
             
             uploaded += 1
             print(f"  [{idx}/{total}] Upload thanh cong: {title}")
